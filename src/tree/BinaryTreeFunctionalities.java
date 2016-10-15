ce7ab1ccd5024d77511a2def6f33507892c74936
nazarmubeen <fam.nazar@gmail.com>
2016-10-15 23:28:04 +0530

    Tree programs

---
+++ src/tree/BinaryTreeFunctionalities.java
@@ -0,0 +1,82 @@
+package tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTreeFunctionalities {
+
+	//height of a tree
+	 public static int height(Node root)
+	    {
+
+		    if(root==null)
+	          {
+	            return -1;
+	        }
+		      
+			int lheight= height(root.left)+1;
+			
+			int rheight= height(root.right)+1;
+			 
+			return Math.max(lheight, rheight);
+}
+	//Getting top view of the tree 
+	 void top_view(Node root)
+	 {
+	          goLeft(root.left);
+	            System.out.print(root.data+" ");
+	 		 goRight(root.right);
+	 }
+
+	  
+	 	 void goLeft(Node root)
+	 	 {
+	 		 if(root.left==null || root.right==null)
+	 		 {
+	              System.out.print(root.data+" ");
+	 			 return;
+	 		 }
+	 		 
+	 		 goLeft(root.left);
+	 		 System.out.print(root.data+" ");
+	 	 }
+	 	 
+	 	 void goRight(Node root)
+	 	 {
+	 		 if(root.left==null || root.right==null)
+	 		 {
+	             System.out.print(root.data+" ");
+	 			 return;
+	 		 }
+	 		 System.out.print(root.data+" ");
+	 		 goRight(root.right);
+	 		 
+	 	 }
+	 	//Getting top view of the tree end
+
+//level order traversal
+	 	 
+	 	void  level_order(Node root)
+	 	{
+
+	 		Queue<Node> q=new LinkedList<Node>();
+	 		
+	 		q.add(root);
+	 		while(!q.isEmpty())
+	 		{
+	 			System.out.print(q.peek().data+" ");
+	 			root= q.poll();
+	 			if(root.left!=null)
+	 			{
+	 			q.add(root.left);
+	 			}
+	 			if(root.right!=null)
+	 			{
+	 			q.add(root.right);
+	 			}
+	 		}
+      
+	 		
+	 	}
+
+}
---
+++ src/tree/TraverseTree.java
@@ -0,0 +1,47 @@
+package tree;
+
+public class TraverseTree {
+
+	//preorder
+	public static void preOrder(Node root) {
+
+		if(root==null)
+		{
+			return;
+		}
+		
+		System.out.print(root.data+" ");
+		preOrder(root.left);
+		preOrder(root.right);
+		
+	}
+	//preorder
+	public static void postOrder(Node root) {
+
+		if(root==null)
+		{
+			return;
+		}
+		
+		
+		postOrder(root.left);
+		postOrder(root.right);
+		System.out.print(root.data+" ");
+		
+	}
+	//inorder
+		public static void inOrder(Node root) {
+
+			if(root==null)
+			{
+				return;
+			}
+			
+			
+			inOrder(root.left);
+			System.out.print(root.data+" ");
+			inOrder(root.right);
+			
+			
+		}
+}
