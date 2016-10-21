7bab04a934df6dd33868598693aa89334638b23b
nazarmubeen <fam.nazar@gmail.com>
2016-10-21 09:56:33 +0530

    java basics

--- src/tree/SwapNodes.java
+++ src/tree/SwapNodes.java
@@ -1,5 +1,7 @@
 package tree;
 
+import java.util.Scanner;
+
 /*
  * You are given a tree of N nodes where nodes are 
  * indexed from [1..N] and it is rooted at 1. You have
@@ -8,10 +10,49 @@ package tree;
  *  current state of the tree.
  * 
  * 
+3
+2 3
+-1 -1
+-1 -1
+2
+1
+1
  */
+
+//using array implementation
 public class SwapNodes {
 	
+	   static void print(int[] arr)
+       {
+      for(int i=0;i<arr.length;i++)
+           {
+          System.out.println(arr[i]);
+       } 
 
+   }
+
+   public static void main(String[] args) {
+       /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+       Scanner sc=new Scanner(System.in);
+       
+       int[] arr=new int[100];
+       int i=1;
+       while(sc.hasNext())
+       {
+       arr[i]=sc.nextInt();
+     
+       i++;
+       }
+       
+       print(arr);
+       /*  arr[0]=1;
+       for(int i=1;i<arr[i];i++)
+           {
+           arr[i]=sc.nextInt();
+       }
+       print(arr);*/
+       
+   }
 	
 	
 	
