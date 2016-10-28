88c2297dd33c71d6ccad794fecdace4b692a740f
nazarmubeen <fam.nazar@gmail.com>
2016-10-28 11:48:23 +0530

    Exceptions

--- src/com/oops/abstractclasses/Employee.java
+++ src/com/oops/abstractclasses/Employee.java
@@ -69,6 +69,43 @@ public abstract class Employee {
 	public String toString() {
 		return "Employee [Id=" + Id + ", name=" + name + ", hiredate=" + hiredate + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		result = prime * result + ((hiredate == null) ? 0 : hiredate.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		if (hiredate == null) {
+			if (other.hiredate != null)
+				return false;
+		} else if (!hiredate.equals(other.hiredate))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
 	
 	
 	
