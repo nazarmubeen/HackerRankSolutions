f4bdc89bb764a892976318474ed9129de0dbf4cc
mnmubeen <mnmubeen@MNMUBEEN-LAP.oradev.oraclecorp.com>
2017-03-16 15:08:28 +0530

    dynamic programming

---
+++ src/dynamicprogramming/MaxProductSubArray.java
@@ -0,0 +1,52 @@
+package dynamicprogramming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxProductSubArray {
+
+	public static int maxProduct(final List<Integer> a) {
+		int sum=1,maxsum=0;
+		
+		for(int i=0;i<a.size();i++)
+		{
+			sum=a.get(i);
+			if(maxsum<a.get(i))
+			{
+				maxsum=a.get(i);
+			}
+			
+			for(int j=i+1;j<a.size();j++)
+			{
+				sum=a.get(j)*sum;
+				System.out.println("sum="+sum);
+				if(sum>maxsum)
+				{
+					maxsum=sum;
+				}
+				
+				
+			}
+			sum=1;
+			
+		}
+		
+		return maxsum;
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		
+		List<Integer> a=new ArrayList<Integer>();
+		a.add(2);
+		a.add(3);
+		a.add(-2);
+		a.add(4);
+		System.out.println(maxProduct(a));
+		
+		
+		
+		
+	}
+}
