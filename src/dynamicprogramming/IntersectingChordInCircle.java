621fd7020f6231d246d0686a1d0f857309f6fd5c
nazarmubeen <fam.nazar@gmail.com>
2017-10-23 10:30:31 +0530

    Dynamic programming

---
+++ src/dynamicprogramming/IntersectingChordInCircle.java
@@ -0,0 +1,34 @@
+package dynamicprogramming;
+
+public class IntersectingChordInCircle {
+
+
+	
+	public static void main(String[] args)
+	{
+		IntersectingChordInCircle obj=new IntersectingChordInCircle();
+		System.out.println(obj.chordCnt(20));
+		
+	}
+	public int chordCnt(int A) {
+		
+	    int[] temp = new int[A+1];
+        temp[0] = 1;
+        temp[1] = 1;
+        
+        for(int i=2; i<temp.length; i++){
+            for(int j=0; j<i; j++){
+            	System.out.println(" i= "+i);
+            	System.out.println("temp [j] "+temp[j]+" temp[i-j-1] "+temp[i-j-1]);
+                temp[i] += ((long)temp[j]*temp[i-j-1])%1000000007;    
+                if(temp[i]>=1000000007)
+                    temp[i] -= 1000000007;
+    
+            }
+        }
+        return temp[A];
+	}
+	
+
+	
+}
--- src/treeproblems_interviewBit/TreeNode.java
+++ src/treeproblems_interviewBit/TreeNode.java
@@ -2,8 +2,8 @@ package treeproblems_interviewBit;
 
 public class TreeNode {
 
-		int val;
-	    TreeNode left;
-	    TreeNode right;
+		public int val;
+	    public TreeNode left;
+	    public TreeNode right;
 	    TreeNode(int x) { val = x; }
 }
