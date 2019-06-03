3476a92450f93d709e3bae6c47913944269fd94c
nazarmubeen <fam.nazar@gmail.com>
2019-06-03 10:38:25 +0530

    Array questions

--- src/Arrays/RotateArray.java
+++ src/Arrays/RotateArray.java
@@ -23,29 +23,29 @@ public class RotateArray {
 	}
 	
 	public static void rotate(ArrayList<ArrayList<Integer>> a) { 
-		int S = a.size() - 1;
+        int S = a.size() - 1;
     
     for(int i = 0 ; i < a.size() / 2 ; i++){
         
         
         for(int j = i; j < S - i; ++j){
             
-            int temp1 = a.get(S-j).get(i);
-            int temp2 = a.get(S-i).get(S-j);
-            int temp3 = a.get(j).get(S-i);
-            int temp4 = a.get(i).get(j);
+            int temp1 = a.get(i).get(j);
+            int temp2 = a.get(S-j).get(i);
+            int temp3 = a.get(S-i).get(S-j);
+            int temp4 = a.get(j).get(S-i);
             
             
             // swap
-            a.get(i).set(j,temp1);
-            a.get(S-j).set(i,temp2);
-            a.get(S-i).set(S-j,temp3);
-            a.get(j).set(S-i,temp4);
-            System.out.println(" j = "+j);
-            System.out.println("a "+a);
+            a.get(i).set(j,temp2);
+            a.get(S-j).set(i,temp3);
+            a.get(S-i).set(S-j,temp4);
+            a.get(j).set(S-i,temp1);
+        //    System.out.println(" j = "+j);
+          //  System.out.println("a "+a);
         }
         }
-	}
+    }
 	
 	public static void rotate2(ArrayList<ArrayList<Integer>> a) { 
 		int S = a.size() - 1;
