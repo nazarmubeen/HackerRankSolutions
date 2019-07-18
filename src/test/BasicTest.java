f200b5c504221fc51f8427a2a86d9dad00cb84b2
nazarmubeen <fam.nazar@gmail.com>
2019-07-18 19:49:03 +0530

    added junit support to the project

---
+++ src/test/BasicTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class BasicTest {
+
+	@Test
+	public void test() {
+		Basic.basicString();
+		assertNotNull(Basic.getMessage());
+	}
+
+}
