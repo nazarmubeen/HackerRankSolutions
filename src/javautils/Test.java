b565f7af1e3b72baa8b3c6753031cfe9a93903b4
fam.nazar@gmail.com <MInewant@730>
2020-02-29 11:27:13 +0530

    maxsubarray

--- src/javautils/Test.java
+++ src/javautils/Test.java
@@ -3,5 +3,7 @@ package javautils;
 public class Test {
     public static void main(String[] args){
         System.out.println("this is test package for java libraries");
+        System.out.println("this is not good");
+
     }
 }
