c5d2b29572714919f5c73a29e97e0bad7cfdd9a3
nazarmubeen <fam.nazar@gmail.com>
2020-03-31 21:31:27 +0530

    kth largest element

---
+++ 2020/src/search/KthLargestInArray.java
@@ -0,0 +1,75 @@
+package search;
+
+public class KthLargestInArray {
+
+    public static void main(String[] args){
+
+        int[] arr={7, 10, 4, 3, 20, 15};
+        System.out.println(arr);
+        kthSmallest(arr,0,arr.length-1,3);
+
+
+    }
+
+    public static int partition(int [] arr, int l,
+                                int r)
+    {
+        System.out.println(" l "+l+" r "+r);
+        int x = arr[r], i = l;
+        for (int j = l; j <= r - 1; j++)
+        {
+            if (arr[j] <= x)
+            {
+                //Swapping arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+                i++;
+            }
+        }
+
+        //Swapping arr[i] and arr[r]
+        int temp = arr[i];
+        arr[i] = arr[r];
+        arr[r] = temp;
+
+        System.out.println("partition i is "+i);
+
+        return i;
+    }
+
+    // This function returns k'th smallest element
+    // in arr[l..r] using QuickSort based method.
+    // ASSUMPTION: ALL ELEMENTS IN ARR[] ARE DISTINCT
+    public static int kthSmallest(int[] arr, int l,
+                                  int r, int k)
+    {
+        // If k is smaller than number of elements
+        // in array
+        if (k > 0 && k <= r - l + 1)
+        {
+            // Partition the array around last
+            // element and get position of pivot
+            // element in sorted array
+            int pos = partition(arr, l, r);
+
+            // If position is same as k
+            if (pos-l == k-1)
+                return arr[pos];
+
+            // If position is more, recur for
+            // left subarray
+            if (pos-l > k-1)
+                return kthSmallest(arr, l, pos-1, k);
+
+            // Else recur for right subarray
+            return kthSmallest(arr, pos+1, r, k-pos+l-1);
+        }
+
+        // If k is more than number of elements
+        // in array
+        return Integer.MAX_VALUE;
+    }
+
+}
