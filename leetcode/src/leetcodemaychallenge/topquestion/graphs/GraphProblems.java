0219de007f4c5aeb60e22231a8d5a964f28e50ea
nazarmubeen <fam.nazar@gmail.com>
2020-05-12 16:11:18 +0530

    change in directory structure

--- leetcode/src/leetcodemaychallenge/topquestion/graphs/GraphProblems.java
+++ leetcode/src/leetcodemaychallenge/topquestion/graphs/GraphProblems.java
@@ -34,9 +34,35 @@ public class GraphProblems {
         if (row < 0 || col < 0 || row >= H || col >= L || islandGrid[row][col] != '1')
             return;
         islandGrid[row][col] = '0'; //marking it visited
-        DFS(islandGrid, row+ 1, col); // go down
-        DFS(islandGrid, row- 1, col); //go top
+        DFS(islandGrid, row + 1, col); // go down
+        DFS(islandGrid, row - 1, col); //go top
         DFS(islandGrid, row, col + 1); //go right
         DFS(islandGrid, row, col - 1); // go left
+    }
+
+
+//https://leetcode.com/explore/featured/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3326/
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        boolean[][] visited = new boolean[image.length][image[0].length];
+        changeValues(sr, sc, image[sr][sc], image, newColor, visited);
+        return image;
+
+    }
+
+    void changeValues(int x, int y, int oldValue, int[][] image, int newValue, boolean[][] visited) {
+        //    System.out.println("x "+x+" y "+y+"oldvalue "+oldValue+" newvalue "+newValue);
+        if (x < 0 || x >= image.length || y < 0 || y >= image[x].length || oldValue != image[x][y] || visited[x][y]) {
+            return;
+        }
+        visited[x][y] = true;
+        oldValue = image[x][y];
+        image[x][y] = newValue;
+
+        changeValues(x + 1, y, oldValue, image, newValue, visited);
+        changeValues(x - 1, y, oldValue, image, newValue, visited);
+        changeValues(x, y + 1, oldValue, image, newValue, visited);
+        changeValues(x, y - 1, oldValue, image, newValue, visited);
+
     }
 }
