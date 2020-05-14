0d43f4c2962eef58a386e631e4bfafc8cda90047
nazarmubeen <fam.nazar@gmail.com>
2020-05-14 20:57:41 +0530

    change in directory structure

--- leetcode/src/leetcodemaychallenge/topquestion/dp/DPProblems.java
+++ leetcode/src/leetcodemaychallenge/topquestion/dp/DPProblems.java
@@ -1,8 +1,10 @@
 package leetcodemaychallenge.topquestion.dp;
 
+import java.util.Stack;
+
 public class DPProblems {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 
@@ -59,7 +61,7 @@ public class DPProblems {
         int currentProfit=0;
         int buy=prices[0];
 
-        for(int i=1;i<prices.length;i++){
+        for(int i = 1; i<prices.length; i++){
 
 
             if(prices[i]-buy>currentProfit){
@@ -71,12 +73,70 @@ public class DPProblems {
             }
 
 
-
         }
 
         return currentProfit;
 
 
+    }
 
+    public String removeKdigits(String num, int k) {
+        //456654321   k = 3
+        //654321. k = 3
+        //12345654 k = 3
+
+        //1219 k = 1, remove local peak from the highest bit
+
+        StringBuilder sb = new StringBuilder();
+        ArrayDeque<Character> dq = new ArrayDeque<>();
+        int i = 0;
+        while (i < num.length()) {
+            while (k > 0 && dq.size() > 0 && num.charAt(i) < dq.peekLast()) {
+                dq.pollLast();
+                k--;
+            }
+            dq.addLast(num.charAt(i));
+            i++;
+        }
+        while (k > 0 && dq.size() > 0) {
+            dq.pollLast();
+            k--;
+        }
+        while (dq.size() > 0) {
+            char c = dq.pollFirst();
+            if (sb.length() > 0 || c != '0') {
+                sb.append(c);
+            }
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+
+    public String removeKdigits2(String num, int k) {
+        int len = num.length();
+        //corner case
+        if (k == len)
+            return "0";
+
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        while (i < num.length()) {
+            while (k > 0 && !stack.isEmpty() && stack.peek() > num.charAt(i)) {
+                stack.pop();
+                k--;
+            }
+            stack.push(num.charAt(i));
+            i++;
+        }
+        while (k > 0) {
+            stack.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty())
+            sb.append(stack.pop());
+        sb.reverse();
+        while (sb.length() > 1 && sb.charAt(0) == '0')
+            sb.deleteCharAt(0);
+        return sb.toString();
     }
 }
